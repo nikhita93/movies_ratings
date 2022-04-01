7a6dd3b92d9b114dc95303a3a0ab877a9210e78a
nikhita93 <102796449+nikhita93@users.noreply.github.com>
2022-04-01 10:26:12 +0100

    Update Exercise2Candidate.scala

--- src/main/scala/uk/co/newday/solutions/Exercise2Candidate.scala
+++ src/main/scala/uk/co/newday/solutions/Exercise2Candidate.scala
@@ -6,5 +6,6 @@ object Exercise2Candidate {
 
   def execute(movies: DataFrame, ratings: DataFrame): (DataFrame) = {
    val joined_df=ratings_df.join(movies_df,"MovieID")
+    val result_avg = joined_df.groupBy(ratings_df("MovieID")).agg(max("Rating") as "Max",min("Rating") as "Min",avg("Rating") as "avg")
   }
 }
