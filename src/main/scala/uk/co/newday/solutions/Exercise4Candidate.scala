d6f1a1148612a7036c911dd3634436fd21c2f091
nikhita93 <102796449+nikhita93@users.noreply.github.com>
2022-04-01 10:36:04 +0100

    Update Exercise4Candidate.scala
    nikhita's solution

--- src/main/scala/uk/co/newday/solutions/Exercise4Candidate.scala
+++ src/main/scala/uk/co/newday/solutions/Exercise4Candidate.scala
@@ -5,6 +5,9 @@ import org.apache.spark.sql.DataFrame
 object Exercise4Candidate {
 
   def execute(movies: DataFrame, ratings: DataFrame, movieRatings:DataFrame, ratingWithRankTop3:DataFrame) = {
-    //write all the output in parquet format
+  joined_df.write.format("parquet").save("joined_tbl1.parquet")
+  result_rating.write.format("parquet").save("result_rating.parquet")
+  result_avg.write.format("parquet").save("result_avg.parquet")
+
   }
 }
