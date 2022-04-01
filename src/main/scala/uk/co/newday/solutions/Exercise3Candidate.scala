6887d42d98a87e0b5c4e24e2f4f8c8506a80f2fc
nikhita93 <102796449+nikhita93@users.noreply.github.com>
2022-04-01 10:25:19 +0100

    Update Exercise3Candidate.scala
    nikhita's solution

--- src/main/scala/uk/co/newday/solutions/Exercise3Candidate.scala
+++ src/main/scala/uk/co/newday/solutions/Exercise3Candidate.scala
@@ -5,7 +5,6 @@ import org.apache.spark.sql.DataFrame
 object Exercise3Candidate {
 
   def execute(movies: DataFrame, ratings: DataFrame): (DataFrame) = {
-    // Complete the exercise and show the top 3 movies per user.
-    null
+    val result_rating = joined_df.withColumn("rank",row_number().over(Window.partitionBy("UserID").orderBy($"Rating".desc))).where($"Rank"<='3').show()
   }
 }
